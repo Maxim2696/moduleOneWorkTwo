ae6290d077c639cf4089c38ca00d73252b22aac5
Maxim2696 <FerraryF21@yandex.ru>
2025-02-24 01:25:34 +0300

    end_Hippodrome_Log

--- src/test/java/MainTest.java
+++ src/test/java/MainTest.java
@@ -1,2 +1,14 @@
-public class MainTest {
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Disabled;
+import org.junit.jupiter.api.Test;
+
+import java.time.Duration;
+
+@Disabled("Checked main method what it is less 22 second")
+class MainTest {
+
+    @Test
+    void whenMainLess22Sec() {
+        Assertions.assertTimeout(Duration.ofSeconds(22), () -> Main.main(new String[]{}));
+    }
 }
