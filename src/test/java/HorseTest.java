7312caf872affb11275d2d27f38f4761f2860e15
Maxim2696 <FerraryF21@yandex.ru>
2025-02-23 15:48:27 +0300

    end_Horse_Test

--- src/test/java/HorseTest.java
+++ src/test/java/HorseTest.java
@@ -3,14 +3,17 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.ValueSource;
+import org.mockito.MockedStatic;
+import org.mockito.Mockito;
 
 import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.Mockito.only;
 
 class HorseTest {
     Horse horse;
     @BeforeEach
     void init() {
-
+        horse = new Horse("flash", 10, 20);
     }
 
     @Test
@@ -32,7 +35,7 @@ class HorseTest {
     }
 
     @ParameterizedTest
-    @ValueSource(strings = {"", " ", "\t", "\n", "\f", "\r"}) //"0x0B"
+    @ValueSource(strings = {"", " ", "\t", "\n", "\f", "\r", "\u000B"})
     void whenHorseNameInvalidCheckMessage(String name) {
         Throwable throwable = assertThrows(IllegalArgumentException.class,
                 () -> horse = new Horse(name, 0, 0));
@@ -41,6 +44,65 @@ class HorseTest {
 
     @Test
     void whenHorseSpeedNegative() {
-
+        assertThrows(IllegalArgumentException.class, () -> horse = new Horse("ksd", -1, 0));
     }
+
+    @Test
+    void whenHorseSpeedNegativeCheckMessage() {
+        Throwable throwable = assertThrows(IllegalArgumentException.class, () -> horse = new Horse("ksd", -1, 0));
+        assertEquals("Speed cannot be negative.", throwable.getMessage());
+    }
+
+    @Test
+    void whenHorseDistanceNegative() {
+        assertThrows(IllegalArgumentException.class, () -> horse = new Horse("ksd", 5, -2));
+    }
+
+    @Test
+    void whenHorseDistanceNegativeCheckMessage() {
+        Throwable throwable = assertThrows(IllegalArgumentException.class, () -> horse = new Horse("ksd", 6, -1));
+        assertEquals("Distance cannot be negative.", throwable.getMessage());
+    }
+
+    @Test
+    void whenGetName() {
+        assertEquals("flash", horse.getName());
+    }
+
+    @Test
+    void whenGetSpeed() {
+        assertEquals(10, horse.getSpeed());
+    }
+
+    @Test
+    void whenGetDistanceBuilderWithThreeParam() {
+        assertEquals(20, horse.getDistance());
+    }
+
+    @Test
+    void whenGetDistanceBuilderWithTwoParam() {
+        horse = new Horse("flash", 10);
+        assertEquals(0, horse.getDistance());
+    }
+
+    @Test
+    void whenCheckGetRandomDouble() {
+        try (MockedStatic<Horse> horseStatic = Mockito.mockStatic(Horse.class)){
+            horse = new Horse("flash", 10, 10);
+            horse.move();
+            horseStatic.verify(() -> Horse.getRandomDouble(0.2, 0.9), only());
+        }
+    }
+
+    @ParameterizedTest
+    @ValueSource(doubles = {2.0, 5.0})
+    void whenCheckMove(double randDouble) {
+        try (MockedStatic<Horse> horseStatic = Mockito.mockStatic(Horse.class)){
+            horseStatic.when(() -> Horse.getRandomDouble(0.2, 0.9)).thenReturn(randDouble);
+            horse = new Horse("flash", 10, 10);
+            horse.move();
+            assertEquals(horse.getDistance(), 10 + 10 * randDouble);
+        }
+    }
+
 }
