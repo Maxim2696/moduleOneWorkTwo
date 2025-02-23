0b4328afe68d55b220287ce6601d72ee2ae8a60f
Maxim2696 <FerraryF21@yandex.ru>
2025-02-23 16:37:00 +0300

    end_Hippodrome_Test

--- src/test/java/HippodromeTest.java
+++ src/test/java/HippodromeTest.java
@@ -1,2 +1,71 @@
-public class HippodromeTest {
+import org.junit.jupiter.api.Test;
+import org.mockito.ArgumentCaptor;
+import org.mockito.Mockito;
+
+import java.util.ArrayList;
+import java.util.Comparator;
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class HippodromeTest {
+
+    Hippodrome hippodrome;
+
+    @Test
+    void whenHippodromeParamNull() {
+        assertThrows(IllegalArgumentException.class, () -> hippodrome = new Hippodrome(null));
+    }
+
+    @Test
+    void whenHippodromeParamNullCheckedMessage() {
+        Throwable throwable = assertThrows(IllegalArgumentException.class, () -> hippodrome = new Hippodrome(null));
+        assertEquals("Horses cannot be null.", throwable.getMessage());
+    }
+
+    @Test
+    void whenHippodromeParamEmpty() {
+        List<Horse> horses = new ArrayList<>();
+        assertThrows(IllegalArgumentException.class, () -> hippodrome = new Hippodrome(horses));
+    }
+
+    @Test
+    void whenHippodromeParamEmptyCheckedMessage() {
+        List<Horse> horses = new ArrayList<>();
+        Throwable throwable = assertThrows(IllegalArgumentException.class, () -> hippodrome = new Hippodrome(horses));
+        assertEquals("Horses cannot be empty.", throwable.getMessage());
+    }
+
+    @Test
+    void whenGetHorsesList() {
+        List<Horse> horses = new ArrayList<>();
+        for (int i = 0; i < 30; i++) {
+            horses.add(new Horse("" + i, i));
+        }
+        hippodrome = new Hippodrome(horses);
+        assertIterableEquals(horses, hippodrome.getHorses());
+    }
+
+    @Test
+    void whenGetWinnerHorse() {
+        List<Horse> horses = new ArrayList<>();
+        for (int i = 0; i < 30; i++) {
+            horses.add(new Horse("" + i, i));
+        }
+        hippodrome = new Hippodrome(horses);
+        Horse maxDistance = horses.stream().max(Comparator.comparing(Horse::getDistance)).get();
+        assertSame(maxDistance, hippodrome.getWinner());
+    }
+
+    @Test
+    void whenHorseOnHippodromeMove() {
+        List<Horse> horses = new ArrayList<>();
+        Horse horse = Mockito.mock(Horse.class);
+        for (int i = 0; i < 50; i++) {
+            horses.add(horse);
+        }
+        hippodrome = new Hippodrome(horses);
+        hippodrome.move();
+        Mockito.verify(horse, Mockito.times(50)).move();
+    }
 }
